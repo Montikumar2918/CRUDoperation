d87bf3853a746562515fe51e76e3410ab03c43ce
Monti kumar <kumarmonti445@gmail.com>
2021-08-24 00:19:33 -0400

    first commit

--- script.js
+++ script.js
@@ -21,8 +21,7 @@ function createUser({name, avatar, createdAt, id}){
           <div class="details">
          <h3>${name}</h3>
          <p>${new Date(createdAt).toDateString()}</p>
-         <div class="shareButtons">
-         
+         <button onclick="editUser(${id})">Edit User</button>
          <button onclick="deleteUser(${id})">Delete</button>
          
          </div>
@@ -63,10 +62,45 @@ function createUser({name, avatar, createdAt, id}){
                            avatar: avatar,
                            createdAt: new Date().toISOString()
                         }
-                     )
+                     ),
                   }
                );
+               datas = await data.json();
                getUser()
+                document.querySelector(".add-username").value = "";
+                document.querySelector(".add-avatar").value = " ";
+                datas.forEach((update) => {
+                createProfile(update);
+                });
+                }
+                //Edit user
+                async function editUser(id) {
+                  const name = document.querySelector(".add-username").value;
+                  const avatar = document.querySelector(".add-avatar").value;
+                  console.log(name,avatar);
+                  const data = await fetch(
+                     `https://611f26469771bf001785c72e.mockapi.io/users/${id}`,
+                     {
+                        method: "PUT",
+                        headers: {
+                           "Content-Type": "application/json"
+                        },
+                        body:JSON.stringify(
+                           {
+                              name: name,
+                              avatar: avatar,
+                              createdAt: new Date().toISOString()
+                           }
+                        ),
+                     }
+                  );
+                  datas = await data.json();
+                  getUser()
+                  document.querySelector(".add-username").value = "";
+                  document.querySelector(".add-avatar").value = " ";
+                  datas.forEach((update) => {
+                    createProfile(update);
+                  });
                 }
               
          
